cf26dc5ba1f2b63053039ec7e35de882ed377713
Martin Fenner <mf@martinfenner.org>
2013-01-30 20:23:37 +0100

    ALM-178 initial version, based on cookbook in alm repo

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+maintainer        "Martin Fenner"
+maintainer_email  "mfenner@plos.org"
+license           "Apache 2.0"
+description       "Configures ALM server"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "1.0.0"
+depends           "git"
+
+%w{ ubuntu }.each do |os|
+  supports os
+end

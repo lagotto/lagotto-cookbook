62cd3744072db8062154e59c5a8e319386f5f24e
Martin Fenner <mf@martinfenner.org>
2013-01-31 14:31:43 +0100

    ALM-178 further cleanup of default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,18 @@
 require 'securerandom'
 
-# create new database.yml
+# Create new database.yml
 template "/vagrant/config/database.yml" do
   source 'database.yml.erb'
+  owner 'root'
+  group 'root'
+  mode 0644
+end
+
+# Create new settings.yml
+node.set_unless['app']['key'] = SecureRandom.hex(30)
+node.set_unless['app']['secret'] = SecureRandom.hex(30)
+template "/vagrant/config/settings.yml" do
+  source 'settings.yml.erb'
   owner 'root'
   group 'root'
   mode 0644
@@ -13,29 +23,6 @@ execute "create CouchDB database #{node[:couchdb][:database]}" do
   command "curl -X DELETE http://#{node[:couchdb][:host]}:#{node[:couchdb][:port]}/#{node[:couchdb][:database]}/"
   command "curl -X PUT http://#{node[:couchdb][:host]}:#{node[:couchdb][:port]}/#{node[:couchdb][:database]}/"
   ignore_failure true
-end
-
-# Generate new keys unless they have already been stored in settings.yml
-if File.exists? "/vagrant/config/settings.yml"
-  settings = YAML.load_file("/vagrant/config/settings.yml")["defaults"]
-  node.set_unless['app']['key'] = settings["rest_auth_site_key"]
-  node.set_unless['app']['secret'] = settings["session_secret"]
-else
-  # create new settings.yml
-  node.set_unless['app']['key'] = SecureRandom.hex(30)
-  node.set_unless['app']['secret'] = SecureRandom.hex(30)
-  template "/vagrant/config/settings.yml" do
-    source 'settings.yml.erb'
-    owner 'root'
-    group 'root'
-    mode 0644
-  end
-end
-
-# Run bundle command
-bash "run bundle install in app directory" do
-  cwd "/vagrant"
-  code "bundle install"
 end
 
 # Optionally seed the database with sources, groups and sample articles
@@ -50,6 +37,11 @@ end
 bash "rake db:setup RAILS_ENV=#{node[:rails][:environment]}" do
   cwd "/vagrant"
   code "rake db:setup RAILS_ENV=#{node[:rails][:environment]}"
+end
+
+# Run bundle command
+bash "bundle" do
+  code "cd /vagrant && bundle install"
 end
 
 # Generate new Procfile

a075d6a7a6bb4d5bf1b854ad3d14a75c0c7feeee
Martin Fenner <mf@martinfenner.org>
2013-01-31 14:13:31 +0100

    ALM-178 cleaned up recipe

--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,15 @@ maintainer_email  "mfenner@plos.org"
 license           "Apache 2.0"
 description       "Configures ALM server"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.0"
+version           "1.0.1"
+depends           "apt"
+depends           "build-essential"
 depends           "git"
+depends           "passenger_apache2"
+depends           "mysql"
+depends           "couchdb"
+depends           "phantomjs"
 
-%w{ ubuntu }.each do |os|
+%w{ ubuntu centos }.each do |os|
   supports os
 end

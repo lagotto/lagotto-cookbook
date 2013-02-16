c56921dbdc6406c39697e3cc97deeb4ccd1fddc9
Martin Fenner <mf@martinfenner.org>
2013-02-16 13:04:43 +0100

    ALM-174 set many default attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,15 @@
-default[:app][:name] = "alm"
-default[:app][:useragent] = "Article Level Metrics"
-default[:app][:layout] = "greenrobo"
-default[:app][:seed_additional_sources] = true
-default[:app][:seed_sample_articles] = true
+default[:alm][:name] = "alm"
+default[:alm][:host] = "alm.local"
+default[:alm][:useragent] = "Article Level Metrics"
+default[:alm][:email] = "ADMIN@EXAMPLE.COM"
+default[:alm][:layout] = "greenrobo"
+default[:alm][:uid] = "doi"
+default[:alm][:doi_prefix] = ""
+default[:alm][:copernicus] = { "username" => "", "password" => "" }
+default[:alm][:crossref] = { "username" => "", "password" => "" }
+default[:alm][:facebook] = { "access_token" => "" }
+default[:alm][:mendeley] = { "api_key" => "" }
+default[:alm][:nature] = { "api_key" => "" }
+default[:alm][:research_blogging] = { "username" => "", "password" => "" }
+default[:alm][:seed_sample_articles] = true
+default[:alm][:environment] = "development"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,9 +1,12 @@
 if node['platform'] == "ubuntu"
   # Install required packages
-  %w{ruby1.9.3 libxslt-dev libxml2-dev ruby-bundler curl}.each do |pkg|
+  %w{ruby1.9.3 libxslt-dev libxml2-dev curl}.each do |pkg|
     package pkg do
       action :install
     end
+  end
+  gem_package "bundler" do
+    gem_binary "/usr/bin/gem"
   end
 end
 
@@ -16,8 +19,8 @@ end
 
 # Create new settings.yml
 require 'securerandom'
-node.set_unless['app']['key'] = SecureRandom.hex(30)
-node.set_unless['app']['secret'] = SecureRandom.hex(30)
+node.set_unless['alm']['key'] = SecureRandom.hex(30)
+node.set_unless['alm']['secret'] = SecureRandom.hex(30)
 template "/vagrant/config/settings.yml" do
   source 'settings.yml.erb'
   owner 'root'
@@ -42,17 +45,17 @@ template "/vagrant/db/seeds/sources.seeds.erb" do
 end
 
 # Create default databases and run migrations
-script "rake db:setup RAILS_ENV=#{node[:rails][:environment]}" do
+script "rake db:setup RAILS_ENV=#{node[:alm][:environment]}" do
   interpreter "bash"
   cwd "/vagrant"
-  code "rake db:setup RAILS_ENV=#{node[:rails][:environment]}"
+  code "rake db:setup RAILS_ENV=#{node[:alm][:environment]}"
 end
 
 # Create default CouchDB database
-script "create CouchDB database #{node[:couchdb][:database]}" do
+script "create CouchDB database #{node[:alm][:name]}" do
   interpreter "bash"
-  code "curl -X DELETE http://#{node[:couchdb][:host]}:#{node[:couchdb][:port]}/#{node[:couchdb][:database]}/"
-  code "curl -X PUT http://#{node[:couchdb][:host]}:#{node[:couchdb][:port]}/#{node[:couchdb][:database]}/"
+  code "curl -X DELETE http://#{node[:couchdb][:host]}:#{node[:couchdb][:port]}/#{node[:alm][:name]}/"
+  code "curl -X PUT http://#{node[:couchdb][:host]}:#{node[:couchdb][:port]}/#{node[:alm][:name]}/"
   ignore_failure true
 end
 
@@ -64,18 +67,32 @@ template "/vagrant/Procfile" do
   mode 0644
 end
 
-case node[:platform]
-when "debian","ubuntu"
+case node['platform']
+when "ubuntu"
   include_recipe "passenger_apache2::mod_rails"
   
   execute "disable-default-site" do
     command "sudo a2dissite default"
+  end
+
+  web_app "alm" do
+    template "alm.conf.erb"
     notifies :reload, resources(:service => "apache2"), :delayed
   end
-end
-
-web_app "alm" do
-  docroot "/vagrant/public"
-  template "alm.conf.erb"
-  notifies :reload, resources(:service => "apache2"), :delayed
+when "centos"
+  template "/etc/httpd/conf.d/alm.conf" do
+    source 'alm.conf.erb'
+    owner 'root'
+    group 'root'
+    mode 0644
+  end
+  
+  script "start httpd" do
+    interpreter "bash"
+    code "sudo iptables -I INPUT -p tcp --dport 80 -j ACCEPT"
+    code "sudo service iptables save"
+    code "sudo /etc/init.d/iptables restart"
+    code "sudo chkconfig --levels 235 httpd on"
+    code "sudo /sbin/service httpd start"
+  end
 end

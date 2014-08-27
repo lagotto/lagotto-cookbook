19ea22af3974aab71e23638a5afe3ba7ad6000b1
Martin Fenner <mfenner@plos.org>
2014-08-27 02:03:12 +0200

    added dependency for rails cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,12 @@ maintainer_email  "mfenner@plos.org"
 license           "Apache 2.0"
 description       "Configures ALM server"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "2.0.0"
+version           "2.0.1"
 depends           "git"
 depends           "mysql"
 depends           "database"
 depends           "couchdb"
+depends           "rails"
 
 %w{ ubuntu }.each do |platform|
   supports platform

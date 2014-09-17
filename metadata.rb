65eaf58b503ddc421bdfaf4c0536b0fc3411c394
Martin Fenner <mfenner@plos.org>
2014-09-17 19:55:33 +0200

    don't require phantomjs, will be installed via gem

--- metadata.rb
+++ metadata.rb
@@ -4,13 +4,12 @@ maintainer_email  "mfenner@plos.org"
 license           "Apache 2.0"
 description       "Install and configure the Lagotto application"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "3.4.11"
+version           "3.4.12"
 
 # opscode cookbooks
 depends           "apt"
 depends           "memcached"
 depends           "postfix"
-depends           "phantomjs", "~> 1.0.3"
 depends           "mysql", '~> 5.4.3'
 depends           "database"
 depends           "couchdb"

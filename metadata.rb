b1efcca97dc15b0eb04a0d0568027a07c62ebd79
Martin Fenner <mfenner@plos.org>
2014-10-21 15:15:18 +0200

    use dotenv and ENV variables

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "mfenner@plos.org"
 license           "Apache 2.0"
 description       "Install and configure the Lagotto application"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "3.5.8"
+version           "3.6.0"
 
 # opscode cookbooks
 depends           "apt"
@@ -17,6 +17,7 @@ depends           "couchdb"
 
 # our own cookbooks
 depends           "ruby", "~> 0.4.0"
+depends           "dotenv", "~> 0.1.0"
 depends           "passenger_nginx", "~> 0.3.0"
 depends           "mysql_rails", "~> 0.3.0"
 depends           "capistrano", "~> 0.6.0"
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,12 +7,15 @@ include_recipe "phantomjs"
 
 # load .env configuration file with ENV variables
 # copy configuration file to shared folder
-capistrano_env 'lagotto' do
+dotenv 'lagotto' do
   action          [:load, :copy]
 end
 
 # install mysql and create configuration file and database
 mysql_rails 'lagotto' do
+  username        ENV['DB_USERNAME']
+  password        ENV['DB_PASSWORD']
+  host            ENV['DB_HOST']
   action          :create
 end
 
@@ -24,10 +27,16 @@ end
 
 # install nginx and create configuration file and application root
 passenger_nginx 'lagotto' do
+  user            ENV['DEPLOY_USER']
+  group           ENV['DEPLOY_GROUP']
+  rails_env       ENV['RAILS_ENV']
   action          :config
 end
 
 # create required files and folders, and deploy application
 capistrano 'lagotto' do
+  user            ENV['DEPLOY_USER']
+  group           ENV['DEPLOY_GROUP']
+  rails_env       ENV['RAILS_ENV']
   action          [:config, :bundle_install, :precompile_assets, :migrate, :restart]
 end

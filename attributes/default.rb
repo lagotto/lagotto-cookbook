5258197f736adc8582efa592a36317a51e6357f5
Martin Fenner <mfenner@plos.org>
2014-10-22 01:24:37 +0200

    phantoms settings are not picked up from default attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,1 @@
 default['ruby']['packages'] = %w{ curl git libmysqlclient-dev libpq-dev nodejs avahi-daemon libnss-mdns }
-
-default['phantomjs']['version'] = "1.9.7"
-default['phantomjs']['base_url'] = "https://bitbucket.org/ariya/phantomjs/downloads"

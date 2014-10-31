22a640bb4cd5063975837013df61e908e648a2c9
Martin Fenner <mfenner@plos.org>
2014-10-31 08:40:07 +0100

    use DOTENV variable to specify .env file

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,7 +8,7 @@ include_recipe "phantomjs"
 # load .env configuration file with ENV variables
 # copy configuration file to shared folder
 dotenv 'lagotto' do
-  app_env         ENV['APP_ENV']
+  dotenv         ENV['DOTENV']
   action          :nothing
 end.run_action(:load)
 

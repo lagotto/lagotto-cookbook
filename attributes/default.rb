82888ceaeeaf85cefeb53768a9cdd08c321d7ea3
Martin Fenner <mfenner@plos.org>
2014-11-29 15:10:55 +0100

    npm_packages as array of hashes with name attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,6 @@
 default['ruby']['packages'] = %w{ curl git libmysqlclient-dev libpq-dev avahi-daemon libnss-mdns python-software-properties software-properties-common }
-default['nodejs']['npm_packages'] = %w{ bower ember-cli phantomjs }
+default['nodejs']['npm_packages'] = [{ "name" => "bower" },
+                                     { "name" => "ember-cli" },
+                                     { "name" => "phantomjs" }]
 default["dotenv"] = "default"
 default["application"] = "lagotto"

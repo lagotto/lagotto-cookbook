41d30215e825185d492387d56bf75971225e52eb
Martin Fenner <mfenner@plos.org>
2014-11-29 14:42:27 +0100

    go pack to installing npmspackages via Chef recipe

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "mfenner@plos.org"
 license           "Apache 2.0"
 description       "Install and configure the Lagotto application"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "4.0.9"
+version           "4.1.0"
 
 # opscode cookbooks
 depends           "apt"
@@ -17,6 +17,7 @@ depends           "nodejs"
 
 # our own cookbooks
 depends           "ruby", "~> 0.6.0"
+depends           "ember_cli", "~> 0.1.0"
 depends           "dotenv", "~> 0.1.0"
 depends           "passenger_nginx", "~> 0.3.0"
 depends           "mysql_rails", "~> 0.3.0"

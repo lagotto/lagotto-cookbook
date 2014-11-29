ddb3eb51553cbd980db66c07ca066041888c7c9b
Martin Fenner <mfenner@plos.org>
2014-11-29 12:56:38 +0100

    include npm install via package.json

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default['ruby']['packages'] = %w{ curl git libmysqlclient-dev libpq-dev avahi-daemon libnss-mdns python-software-properties software-properties-common }
-default['nodejs']['npm_packages'] = %w{ bower ember-cli phantomjs broccoli-sass }
+default['nodejs']['npm_packages'] = %w{ bower ember-cli phantomjs }
 default["dotenv"] = "default"
 default["application"] = "lagotto"

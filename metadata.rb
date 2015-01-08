2a53564482b50840fe0427e1cda53e3f29289661
Martin Fenner <mfenner@plos.org>
2015-01-08 09:35:21 +0100

    use updated dotenv cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "mfenner@plos.org"
 license           "Apache 2.0"
 description       "Install and configure the Lagotto application"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "4.4.2"
+version           "4.4.3"
 
 # opscode cookbooks
 depends           "apt"
@@ -18,7 +18,7 @@ depends           "nodejs"
 
 # our own cookbooks
 depends           "ruby", "~> 0.6.0"
-depends           "dotenv", "~> 0.1.0"
+depends           "dotenv", "~> 0.2.0"
 depends           "passenger_nginx", "~> 0.4.0"
 depends           "mysql_rails", "~> 0.3.0"
 depends           "capistrano", "~> 0.8.0"

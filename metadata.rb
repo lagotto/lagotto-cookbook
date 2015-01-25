30eab864afbab5001a881f108b00ae962d664890
Martin Fenner <mfenner@plos.org>
2015-01-25 18:49:44 +0100

    updated dependency on database cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,14 +4,14 @@ maintainer_email  "mfenner@plos.org"
 license           "Apache 2.0"
 description       "Install and configure the Lagotto application"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "4.4.4"
+version           "4.4.5"
 
 # opscode cookbooks
 depends           "apt"
 depends           "memcached"
 depends           "postfix"
 depends           "mysql", "~> 6.0"
-depends           "database"
+depends           "database", "~> 3.0.3"
 depends           "couchdb"
 depends           "redisio"
 depends           "nodejs"

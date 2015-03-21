1c1635e269e84208877ea1b5f249afc38badd58b
Martin Fenner <mfenner@plos.org>
2015-03-21 16:55:29 +0100

    added code-climate npm package

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,4 +9,4 @@ default['nodejs']['install_method'] = "binary"
 default['nodejs']['version'] = "0.10.33"
 default['nodejs']['binary']['checksum']['linux_x64'] = "159e5485d0fb5c913201baae49f68fd428a7e3b08262e9bf5003c1b399705ca8"
 default['nodejs']['npm']['version'] = "1.4.28"
-default['npm_packages'] = %w{ istanbul }
+default['npm_packages'] = %w{ istanbul codeclimate-test-reporter }

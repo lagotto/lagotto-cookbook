69a5a1d74e705a297b30d2165d1c626009ca713a
Martin Fenner <mfenner@plos.org>
2015-03-21 20:00:12 +0100

    install phantomjs via npm

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,4 +9,6 @@ default['nodejs']['install_method'] = "binary"
 default['nodejs']['version'] = "0.10.33"
 default['nodejs']['binary']['checksum']['linux_x64'] = "159e5485d0fb5c913201baae49f68fd428a7e3b08262e9bf5003c1b399705ca8"
 default['nodejs']['npm']['version'] = "1.4.28"
-default['nodejs']['npm_packages'] = [{ "name" => "istanbul"}, { "name" => "codeclimate-test-reporter" }]
+default['nodejs']['npm_packages'] = [{ "name" => "phantomjs" },
+                                     { "name" => "istanbul"},
+                                     { "name" => "codeclimate-test-reporter" }]

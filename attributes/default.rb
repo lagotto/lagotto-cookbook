f39e6cc81ce3154254cc675cebd9cc81062b8e7e
Martin Fenner <mfenner@plos.org>
2015-03-21 11:17:54 +0100

    install npm and istanbul npm package

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,12 @@
-default['ruby']['packages'] = %w{ curl git libmysqlclient-dev libpq-dev avahi-daemon libnss-mdns python-software-properties software-properties-common }
-default['nodejs']['npm_packages'] = [{ "name" => "bower" },
-                                     { "name" => "ember-cli" },
-                                     { "name" => "phantomjs" }]
+default['ruby']['packages'] = %w{ curl git libmysqlclient-dev python-software-properties software-properties-common }
+default['ruby']['packages'] += %w{ avahi-daemon libnss-mdns } if ENV['RAILS_ENV'] != "production"
 default["dotenv"] = "default"
 default["application"] = "lagotto"
 default['ruby']['merge_slashes_off'] = true
 default['ruby']['api_only'] = false
 default['couch_db']['config']['httpd']['bind_address'] = "0.0.0.0" if ENV['RAILS_ENV'] != "production"
+default['nodejs']['install_method'] = "binary"
+default['nodejs']['version'] = "0.10.33"
+default['nodejs']['binary']['checksum']['linux_x64'] = "159e5485d0fb5c913201baae49f68fd428a7e3b08262e9bf5003c1b399705ca8"
+default['nodejs']['npm']['version'] = "1.4.28"
+default['npm_packages'] = %w{ istanbul }

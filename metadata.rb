4d3eac5435fe0d8f83151d6699e66290860d1bcf
Martin Fenner <mfenner@plos.org>
2015-03-29 18:31:22 +0200

    use newer version of capistrano cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "mfenner@plos.org"
 license           "Apache 2.0"
 description       "Install and configure the Lagotto application"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "4.5.10"
+version           "4.6.0"
 
 # opscode cookbooks
 depends           "apt"
@@ -21,7 +21,7 @@ depends           "ruby", "~> 0.6.0"
 depends           "dotenv", "~> 0.2.0"
 depends           "passenger_nginx", "~> 0.5.0"
 depends           "mysql_rails", "~> 0.3.0"
-depends           "capistrano", "~> 0.8.0"
+depends           "capistrano", "~> 0.9.0"
 
 %w{ ubuntu }.each do |platform|
   supports platform

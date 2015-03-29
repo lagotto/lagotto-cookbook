06ed2aac543220ae805b885d24079164ba5dc8ce
Martin Fenner <mfenner@plos.org>
2015-03-29 23:37:12 +0200

    don't run bower install

--- recipes/default.rb
+++ recipes/default.rb
@@ -42,5 +42,5 @@ capistrano ENV['APPLICATION'] do
   user            ENV['DEPLOY_USER']
   group           ENV['DEPLOY_GROUP']
   rails_env       ENV['RAILS_ENV']
-  action          [:config, :bundle_install, :npm_install, :bower_install, :precompile_assets, :migrate, :restart]
+  action          [:config, :bundle_install, :npm_install, :precompile_assets, :migrate, :restart]
 end

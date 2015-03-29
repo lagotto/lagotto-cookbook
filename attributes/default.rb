f4f4e25e8919268098a398eeb8433377edfe2489
Martin Fenner <mfenner@plos.org>
2015-03-29 17:26:57 +0200

    install nodejs as binary

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,7 @@ default["application"] = "lagotto"
 default['ruby']['merge_slashes_off'] = true
 default['ruby']['api_only'] = false
 default['couch_db']['config']['httpd']['bind_address'] = "0.0.0.0" if ENV['RAILS_ENV'] != "production"
-#default['nodejs']['install_method'] = "binary"
+default['nodejs']['install_method'] = "binary"
 default['nodejs']['version'] = "0.12.1"
 # default['nodejs']['binary']['checksum']['linux_x64'] = "159e5485d0fb5c913201baae49f68fd428a7e3b08262e9bf5003c1b399705ca8"
 default['nodejs']['npm']['version'] = "2.7.5"

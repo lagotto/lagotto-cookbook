f5eb1554fe74dedf76aa3d76e2a78ea2e1bd08b4
Martin Fenner <mfenner@plos.org>
2015-07-02 18:55:48 -0700

    install nodejs via binary method

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,8 +5,8 @@ default["application"] = "lagotto"
 default['ruby']['merge_slashes_off'] = true
 default['ruby']['api_only'] = false
 default['couch_db']['config']['httpd']['bind_address'] = "0.0.0.0" if ENV['RAILS_ENV'] != "production"
-#default['nodejs']['install_method'] = "binary"
-default['nodejs']['version'] = "0.12.0"
+default['nodejs']['install_method'] = "binary"
+#default['nodejs']['version'] = "0.12.0"
 # default['nodejs']['binary']['checksum']['linux_x64'] = "305bf2983c65edea6dd2c9f3669b956251af03523d31cf0a0471504fd5920aac"
 default['nodejs']['npm']['version'] = "2.7.5"
 default['nodejs']['npm_packages'] = [{ "name" => "phantomjs" },

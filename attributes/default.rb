a4569e731704f3ec8213b4e2421939d44a8df719
Martin Fenner <mfenner@plos.org>
2015-07-02 19:23:50 -0700

    add checksum for nodejs binary

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,7 +7,7 @@ default['ruby']['api_only'] = false
 default['couch_db']['config']['httpd']['bind_address'] = "0.0.0.0" if ENV['RAILS_ENV'] != "production"
 default['nodejs']['install_method'] = "binary"
 default['nodejs']['version'] = "0.12.5"
-#default['nodejs']['binary']['checksum']['linux_x64'] = "305bf2983c65edea6dd2c9f3669b956251af03523d31cf0a0471504fd5920aac"
+default['nodejs']['binary']['checksum']['linux_x64'] = "d4d7efb9e1370d9563ace338e01f7be31df48cf8e04ad670f54b6eb8a3c54e03"
 default['nodejs']['npm']['version'] = "2.7.5"
 default['nodejs']['npm_packages'] = [{ "name" => "phantomjs" },
                                      { "name" => "istanbul"},

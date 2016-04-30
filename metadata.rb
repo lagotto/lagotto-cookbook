cde8bca1d4cadfdc2e8f8e2be50c3d74bba9a13b
Martin Fenner <martin.fenner@datacite.org>
2016-04-30 14:03:02 +0200

    no longer install postfix

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,11 @@ maintainer_email  "mfenner@datacite.org"
 license           "Apache 2.0"
 description       "Install and configure the Lagotto application"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "5.0.14"
+version           "5.1"
 
 # opscode cookbooks
 depends           "apt"
 depends           "memcached"
-depends           "postfix"
 depends           "nodejs"
 depends           "consul"
 depends           "rsyslog"
